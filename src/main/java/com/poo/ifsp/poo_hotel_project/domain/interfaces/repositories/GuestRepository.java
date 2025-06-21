f0f590ffaeee55d685de6e503468c23f30e8e32b
vitingr <vitorgabrielsbo1460@gmail.com>
2025-06-21 16:13:18 -0300

    feat(application.properties): added new env files

--- src/main/java/com/poo/ifsp/poo_hotel_project/domain/interfaces/repositories/GuestRepository.java
+++ src/main/java/com/poo/ifsp/poo_hotel_project/domain/interfaces/repositories/GuestRepository.java
@@ -2,8 +2,10 @@ package com.poo.ifsp.poo_hotel_project.domain.interfaces.repositories;
 
 import com.poo.ifsp.poo_hotel_project.domain.entities.Guest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface GuestRepository extends JpaRepository<Guest, UUID> {
 }
